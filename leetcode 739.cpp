34f15228b6daed0ebc02fde27c04ed0b48afa049
Kamlesh Singh mahar <113906986+Kamlesh-Mahar@users.noreply.github.com>
2023-05-09 18:20:00 +0530

    Add files via upload

---
+++ leetcode 739.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> ans(n, 0);
+        stack<int> s;
+        for (int i = n - 1; i >= 0; i--) {
+            while (!s.empty() && temperatures[i] >= temperatures[s.top()]) {
+                s.pop();
+            }
+            if (!s.empty()) {
+                ans[i] = s.top() - i;
+            }
+            s.push(i);
+        }
+        return ans;
+    }
+};
