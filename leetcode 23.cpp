015bccd1a9563f1c2ca24d091121aa3c25d0eefa
Kamlesh Singh mahar <113906986+Kamlesh-Mahar@users.noreply.github.com>
2023-05-21 18:04:50 +0530

    Add files via upload

---
+++ leetcode 23.cpp
@@ -0,0 +1,33 @@
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        auto comp = [](ListNode* a, ListNode* b) {
+            return a->val > b->val;
+        };
+        
+        priority_queue<ListNode*, vector<ListNode*>, decltype(comp)> pq(comp);
+        for (ListNode* list : lists) {
+            if (list) {
+                pq.push(list);
+            }
+        }
+        
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+        
+        while (!pq.empty()) {
+            ListNode* node = pq.top();
+            pq.pop();
+            
+            tail->next = node;
+            tail = tail->next;
+            
+            if (node->next) {
+                pq.push(node->next);
+            }
+        }
+        
+        return dummy.next;
+    }
+};
