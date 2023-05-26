bead7883b3030977da630eb00cd94a770b25e471
Kamlesh Singh mahar <113906986+Kamlesh-Mahar@users.noreply.github.com>
2023-05-26 16:57:30 +0530

    Add files via upload

---
+++ leetcode 328.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+
+        ListNode* oddHead = head;
+        ListNode* evenHead = head->next;
+        ListNode* odd = oddHead;
+        ListNode* even = evenHead;
+
+        while (even != nullptr && even->next != nullptr) {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+
+        odd->next = evenHead;
+
+        return oddHead;
+    }
+};
