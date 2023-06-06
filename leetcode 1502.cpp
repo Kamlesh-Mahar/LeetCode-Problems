9b80141293b276ce2490f797432ae6a11d6050b0
Kamlesh Singh mahar <113906986+Kamlesh-Mahar@users.noreply.github.com>
2023-06-06 15:15:21 +0530

    Add files via upload

---
+++ leetcode 1502.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    bool canMakeArithmeticProgression(std::vector<int>& arr) {
+        std::sort(arr.begin(), arr.end()); 
+        
+        int diff = arr[1] - arr[0];
+        
+        for (int i = 2; i < arr.size(); i++) {
+            if (arr[i] - arr[i-1] != diff) {
+                return false; return false;
+            }
+        }
+        
+        return true; 
+    }
+};
