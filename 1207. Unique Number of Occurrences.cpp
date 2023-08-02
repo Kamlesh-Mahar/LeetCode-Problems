6639aec54392ab8a58e4e5d943cdfd10bdabf4ef
Kamlesh Singh mahar <113906986+Kamlesh-Mahar@users.noreply.github.com>
2023-08-02 23:14:35 +0530

    Add files via upload

---
+++ 1207. Unique Number of Occurrences.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        int i = 0;
+        sort(arr.begin(),arr.end());
+        vector<int> ans;
+        while (i < arr.size()){
+            int count = 1;
+            for (int j = i+1; j< arr.size(); j++){
+                if (arr[i] == arr[j])
+                    count++;
+            }
+            ans.push_back(count);
+            i = i + count;
+        }
+        sort(ans.begin(),ans.end());
+        for (int i = 0; i < ans.size() - 1; i++){
+            if (ans[i] == ans [i+1])
+                return false;
+        }
+        return true;
+    }
+};
