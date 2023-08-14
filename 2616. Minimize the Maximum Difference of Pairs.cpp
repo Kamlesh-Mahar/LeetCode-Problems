36ae8d58ae48cf45cc2762d5052589879dbe7537
Kamlesh Singh mahar <113906986+Kamlesh-Mahar@users.noreply.github.com>
2023-08-14 16:22:23 +0530

    Add files via upload

---
+++ 2616. Minimize the Maximum Difference of Pairs.cpp
@@ -0,0 +1,26 @@
+class Solution {
+ public:
+  int minimizeMax(vector<int>& nums, int p) {
+    sort(nums.begin(), nums.end());
+    int l = 0;
+    int r = nums.back() - nums.front();
+    while (l < r) {
+      const int m = (l + r) / 2;
+      if (numPairs(nums, m) >= p)
+        r = m;
+      else
+        l = m + 1;
+    }
+    return l;
+  }
+ private:
+  int numPairs(const vector<int>& nums, int maxDiff) {
+    int pairs = 0;
+    for (int i = 1; i < nums.size(); ++i)
+      if (nums[i] - nums[i - 1] <= maxDiff) {
+        ++pairs;
+        ++i;
+      }
+    return pairs;
+  }
+};
