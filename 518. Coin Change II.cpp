e730647b506ea4bc9618b25ea8d18ba466fd9bb0
Kamlesh Singh mahar <113906986+Kamlesh-Mahar@users.noreply.github.com>
2023-08-14 16:23:32 +0530

    Add files via upload

---
+++ 518. Coin Change II.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;  
+
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; ++i) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+};
