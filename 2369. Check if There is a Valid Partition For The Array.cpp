97bd0c993a64d846061a195ae3c4158a40abb357
Kamlesh Singh mahar <113906986+Kamlesh-Mahar@users.noreply.github.com>
2023-08-14 16:22:39 +0530

    Add files via upload

---
+++ 2369. Check if There is a Valid Partition For The Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+ public:
+  bool validPartition(vector<int>& nums) {
+    const int n = nums.size();
+    vector<bool> dp(n + 1);
+    dp[0] = true;
+    dp[2] = nums[0] == nums[1];
+
+    for (int i = 3; i <= n; ++i)
+      dp[i] = (dp[i - 2] && nums[i - 2] == nums[i - 1]) ||
+              (dp[i - 3] &&
+               ((nums[i - 3] == nums[i - 2] && nums[i - 2] == nums[i - 1]) ||
+                (nums[i - 3] + 1 == nums[i - 2] &&
+                 nums[i - 2] + 1 == nums[i - 1])));
+    return dp[n];
+  }
+};
