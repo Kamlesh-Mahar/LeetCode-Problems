d770e68c6e54868598c3261e340be7b4d3e6f2a7
Kamlesh Singh mahar <113906986+Kamlesh-Mahar@users.noreply.github.com>
2023-08-15 11:16:11 +0530

    Add files via upload

---
+++ 63. Unique Paths II.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+        vector<vector<long long>> dp(m, vector<long long>(n, 0));
+        dp[0][0] = 1;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    if (i > 0) {
+                        dp[i][j] += dp[i - 1][j];
+                    }
+                    if (j > 0) {
+                        dp[i][j] += dp[i][j - 1];
+                    }
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
