46c8124c29c9065aa1c15b02dbb6ae78c499f442
Kamlesh Singh mahar <113906986+Kamlesh-Mahar@users.noreply.github.com>
2023-08-15 11:15:48 +0530

    Add files via upload

---
+++ 139. Word Break.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        int n = s.length();
+        vector<bool> dp(n + 1, false);
+        dp[0] = true;
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (dp[j] && dict.count(s.substr(j, i - j))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
