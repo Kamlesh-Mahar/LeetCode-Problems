e1a4b845c2142e3b1cdaf021c994a9bc05133970
Kamlesh Singh mahar <113906986+Kamlesh-Mahar@users.noreply.github.com>
2023-09-05 20:38:08 +0530

    Add files via upload

---
+++ 134. Gas Station.cpp
@@ -0,0 +1,21 @@
+class Solution {
+ public:
+  int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+    const int gasses = accumulate(gas.begin(), gas.end(), 0);
+    const int costs = accumulate(cost.begin(), cost.end(), 0);
+    if (gasses - costs < 0)
+      return -1;
+
+    int ans = 0;
+    int sum = 0;
+    for (int i = 0; i < gas.size(); ++i) {
+      sum += gas[i] - cost[i];
+      if (sum < 0) {
+        sum = 0;
+        ans = i + 1;  
+      }
+    }
+
+    return ans;
+  }
+};
