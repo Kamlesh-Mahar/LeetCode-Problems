410ba3abde334103a99fe9ae66b15c102b73132e
Kamlesh Singh mahar <113906986+Kamlesh-Mahar@users.noreply.github.com>
2023-09-09 21:11:32 +0530

    Add files via upload

---
+++ 377. Combination Sum IV.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<unsigned int> dp(target + 1, 0);
+        dp[0] = 1;
+
+        for (int i = 1; i <= target; ++i) {
+            for (int num : nums) {
+                if (i >= num) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+
+        return dp[target];
+    }
+};
