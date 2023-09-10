2b7c95edc265045538edc3cebf27fb119da48c64
Kamlesh Singh mahar <113906986+Kamlesh-Mahar@users.noreply.github.com>
2023-09-10 09:54:46 +0530

    Add files via upload

---
+++ 53. Maximum Subarray.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = nums[0]; 
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.size(); ++i) {
+            currentSum = max(nums[i], currentSum + nums[i]);
+            maxSum = max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+};
