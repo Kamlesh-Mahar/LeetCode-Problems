97bbc5799cd72e45fc49973367cc01b552399f73
Kamlesh Singh mahar <113906986+Kamlesh-Mahar@users.noreply.github.com>
2023-09-11 18:58:00 +0530

    Add files via upload

---
+++ 334. Increasing Triplet Subsequence.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int first = INT_MAX;  
+        int second = INT_MAX; 
+
+        for (int num : nums) {
+            if (num <= first) {
+                first = num; 
+            } else if (num <= second) {
+                second = num; 
+            } else {
+                return true;
+            }
+        }
+
+        return false; 
+    }
+};
