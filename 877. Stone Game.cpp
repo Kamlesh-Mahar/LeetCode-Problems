be104966c83854b8ec6355038867ecf13f5227df
Kamlesh Singh mahar <113906986+Kamlesh-Mahar@users.noreply.github.com>
2023-09-13 16:10:10 +0530

    Add files via upload

---
+++ 877. Stone Game.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+        vector<vector<int>> dp(n, vector<int>(n));
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = piles[i];
+        }
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                dp[i][j] = max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][n - 1] > 0;
+    }
+};
