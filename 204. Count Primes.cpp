6eb36b8464424d57f324767099013b2881b485eb
Kamlesh Singh mahar <113906986+Kamlesh-Mahar@users.noreply.github.com>
2023-09-24 18:47:58 +0530

    Create 204. Count Primes.cpp

---
+++ 204. Count Primes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n <= 2) return 0; 
+
+        vector<bool> isPrime(n, true); 
+        isPrime[0] = isPrime[1] = false; 
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        return count(isPrime.begin() + 2, isPrime.end(), true);
+    }
+};
