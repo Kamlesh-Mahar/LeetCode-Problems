97a80aff88f0b4d109f4d7e93cc271860170cf55
Kamlesh Singh mahar <113906986+Kamlesh-Mahar@users.noreply.github.com>
2024-01-18 18:35:30 +0530

    Create 70. Climbing Stairs.cpp

---
+++ 70. Climbing Stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        int prev1 = 1;
+        int prev2 = 2;
+
+        for (int i = 3; i <= n; ++i) {
+            int current = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = current;
+        }
+
+        return prev2;
+    }
+};
